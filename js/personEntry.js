6f7b9357088938b1d523069953de03951faaa6a5
GeorgeC <y29cheng@gmail.com>
2012-06-21 12:55:41 -0400

    added wiping of content node

--- js/personEntry.js
+++ js/personEntry.js
@@ -1,5 +1,5 @@
-define(["dojo/_base/declare", "dojo/parser", "dojo/ready", "dojo/_base/fx", "dojo/dom-style", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dojo/text!templates/personWidget.html"], 
-		function(declare, parser, ready, baseFx, domStyle, _WidgetBase, _TemplatedMixin, template) {
+define(["dojo/_base/declare", "dojo/parser", "dojo/ready", "dojo/_base/fx", "dojo/fx", "dojo/dom-style", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dojo/text!templates/personWidget.html"], 
+		function(declare, parser, ready, baseFx, fx, domStyle, _WidgetBase, _TemplatedMixin, template) {
 			return declare([_WidgetBase, _TemplatedMixin], {
 				templateString: template,
 				baseClass: "personEntry",
@@ -19,15 +19,24 @@ define(["dojo/_base/declare", "dojo/parser", "dojo/ready", "dojo/_base/fx", "doj
 						duration: 500
 					}).play();
 				},
+				_expandNode: function() {
+					fx.wipeIn({ node: this.bioNode }).play();
+				},
+				_shrinkNode: function() {
+					fx.wipeOut({ node: this.bioNode }).play();
+				},
 				postCreate: function() {
 					var domNode = this.domNode;
 					
 					domStyle.set(domNode, "backgroundColor", this.baseBackgroundColor);
+					domStyle.set(this.bioNode, "height", "0");
 					this.connect(domNode, "onmouseenter", function(e) {
 						this._changeBackgroundColor(this.mouseBackgroundColor);
+						this._expandNode();
 					});
 					this.connect(domNode, "onmouseleave", function(e) {
 						this._changeBackgroundColor(this.baseBackgroundColor);
+						this._shrinkNode();
 					});
 				}
 			});
